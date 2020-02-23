9e490ebb9a70a2fa232dd44d895aff10b6396234
Viliam Repan <vilo.repan@evolveum.com>
2020-02-23 22:13:12 +0100

    improved compatibility with 2019.1.4

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/BrowseTableModel.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/BrowseTableModel.java
@@ -1,6 +1,7 @@
 package com.evolveum.midpoint.studio.ui;
 
 import com.evolveum.midpoint.schema.constants.ObjectTypes;
+import com.evolveum.midpoint.studio.util.ExtendedListSelectionModel;
 import com.evolveum.midpoint.studio.util.MidPointUtils;
 import com.evolveum.midpoint.studio.util.Pair;
 import com.evolveum.midpoint.xml.ns._public.common.common_3.ObjectType;
@@ -133,7 +134,7 @@ public class BrowseTableModel extends AbstractTreeTableModel {
             return selected;
         }
 
-        ListSelectionModel selectionModel = table.getSelectionModel();
+        ExtendedListSelectionModel selectionModel = (ExtendedListSelectionModel) table.getSelectionModel();
         int[] indices = selectionModel.getSelectedIndices();
         for (int i : indices) {
             DefaultMutableTreeTableNode node = (DefaultMutableTreeTableNode) table.getPathForRow(i).getLastPathComponent();
