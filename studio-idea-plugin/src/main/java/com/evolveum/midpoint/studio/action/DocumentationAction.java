60083f5b8c1c192616ec0abbeb86f5fff661dd1a
Viliam Repan <vilo.repan@evolveum.com>
2020-03-01 21:42:49 +0100

    documentation generator using midscribe, more fixes, still not working yet

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/DocumentationAction.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/DocumentationAction.java
@@ -1,12 +1,15 @@
 package com.evolveum.midpoint.studio.action;
 
 import com.evolveum.midpoint.studio.ui.DocumentationDialog;
+import com.evolveum.midscribe.generator.ExportFormat;
 import com.evolveum.midscribe.generator.GenerateOptions;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -16,6 +19,10 @@ public class DocumentationAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent evt) {
         GenerateOptions options = new GenerateOptions();     // todo fix
+        options.setSourceDirectory(new File("./t67/objects"));
+        options.setExportFormat(ExportFormat.HTML);
+        options.setOutput(new File("./t67/example.adoc"));
+
         DocumentationDialog dialog = new DocumentationDialog(options);
         if (!dialog.showAndGet()) {
             return;
@@ -23,7 +30,7 @@ public class DocumentationAction extends AnAction {
 
         options = dialog.getOptions();
 
-//        options.setSourceDirectory(new File("/Users/lazyman/Work/monoted/projects/ek/git/midpoint-project/objects"));
+//        options.setSourceDirectory(new File("./midpoint-project/objects"));
 //        options.getExclude().addAll(Arrays.asList(new String[]{"users/*.xml", "tasks/misc/*"}));
 //        File adoc = new File("./local.adoc");
 //        options.setOutput(adoc);
--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/GenerateDocumentationAction.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/GenerateDocumentationAction.java
@@ -6,6 +6,10 @@ import com.evolveum.midscribe.generator.GenerateOptions;
 import com.evolveum.midscribe.generator.Generator;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.progress.ProgressIndicator;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.ResourceManagerImpl;
+
+import java.util.Properties;
 
 /**
  * Created by Viliam Repan (lazyman).
@@ -24,9 +28,12 @@ public class GenerateDocumentationAction extends BackgroundAction {
 
     @Override
     protected void executeOnBackground(AnActionEvent evt, ProgressIndicator indicator) {
+        Properties properties = new Properties();
+        properties.put(RuntimeConstants.RESOURCE_MANAGER_INSTANCE, new ResourceManagerImpl());
+
         Generator generator = new Generator(options);
         try {
-            generator.generate();
+            generator.generate(properties);
         } catch (Exception ex) {
             MidPointUtils.handleGenericException(evt.getProject(), GenerateDocumentationAction.class,
                     NOTIFICATION_KEY, "Couldn't generate documentation", ex);
