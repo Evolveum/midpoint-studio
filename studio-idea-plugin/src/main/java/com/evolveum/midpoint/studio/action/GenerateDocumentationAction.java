7a75485684fb441d67ccaf0c4363bfcee5861997
Viliam Repan <vilo.repan@evolveum.com>
2020-03-26 15:23:40 +0100

    some logging

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/GenerateDocumentationAction.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/action/GenerateDocumentationAction.java
@@ -5,6 +5,7 @@ import com.evolveum.midpoint.studio.util.MidPointUtils;
 import com.evolveum.midscribe.generator.GenerateOptions;
 import com.evolveum.midscribe.generator.Generator;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.ResourceManagerImpl;
@@ -15,6 +16,8 @@ import java.util.Properties;
  * Created by Viliam Repan (lazyman).
  */
 public class GenerateDocumentationAction extends BackgroundAction {
+
+    private static Logger LOG = Logger.getInstance(GenerateDocumentationAction.class);
 
     private static final String NOTIFICATION_KEY = "Documentation Generator";
 
@@ -35,6 +38,8 @@ public class GenerateDocumentationAction extends BackgroundAction {
         try {
             generator.generate(properties);
         } catch (Exception ex) {
+            LOG.error("Couldn't generate documentation", ex);
+
             MidPointUtils.handleGenericException(evt.getProject(), GenerateDocumentationAction.class,
                     NOTIFICATION_KEY, "Couldn't generate documentation", ex);
         }
