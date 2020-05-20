2891d18718ffdf0fad20a87d429d8fa3fd27d172
Viliam Repan <vilo.repan@evolveum.com>
2020-05-20 14:50:54 +0200

    environment proxy configuration, UI support

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/EnvironmentEditorDialog.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/EnvironmentEditorDialog.java
@@ -1,17 +1,20 @@
 package com.evolveum.midpoint.studio.ui;
 
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.ValidationInfo;
+import com.evolveum.midpoint.client.api.ProxyType;
 import com.evolveum.midpoint.studio.impl.Environment;
 import com.evolveum.midpoint.studio.util.MidPointUtils;
 import com.evolveum.midpoint.studio.util.Selectable;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.ui.ValidationInfo;
 import org.apache.commons.lang.StringUtils;
+import org.jdesktop.swingx.combobox.ListComboBoxModel;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -29,6 +32,11 @@ public class EnvironmentEditorDialog extends DialogWrapper {
     private JButton chooseButton;
     private JLabel colorLabel;
     private JTextField properties;
+    private JTextField proxyHost;
+    private JTextField proxyPort;
+    private JComboBox proxyType;
+    private JPasswordField proxyPassword;
+    private JTextField proxyUsername;
 
     private Selectable<Environment> selectable;
 
@@ -36,11 +44,15 @@ public class EnvironmentEditorDialog extends DialogWrapper {
         super(false);
         setTitle(environment == null ? "Add environment" : "Edit environment");
 
+        colorLabel.setBorder(BorderFactory.createEmptyBorder(0, 3, 0, 0));
+
         if (environment == null) {
             environment = new Selectable<>(new Environment());
             environment.getObject().setAwtColor(MidPointUtils.generateAwtColor());
         }
         this.selectable = environment;
+
+        this.proxyType.setModel(new ListComboBoxModel(Arrays.asList(ProxyType.values())));
 
         fillInFields();
 
@@ -85,6 +97,18 @@ public class EnvironmentEditorDialog extends DialogWrapper {
             infos.add(new ValidationInfo("Username must not be empty"));
         }
 
+        String port = proxyPort.getText();
+        if (port != null) {
+            if (!StringUtils.isNumeric(port)) {
+                infos.add(new ValidationInfo("Proxy port is not a number"));
+            } else {
+                Integer p = Integer.parseInt(port);
+                if (p < 0 || p >= Math.pow(2, 16)) {
+                    infos.add(new ValidationInfo("Proxy port is out of range 0-65535"));
+                }
+            }
+        }
+
         return infos;
     }
 
@@ -105,6 +129,13 @@ public class EnvironmentEditorDialog extends DialogWrapper {
         environment.setUsername(username.getText());
         environment.setPassword(String.copyValueOf(password.getPassword()));
         environment.setIgnoreSslErrors(ignoreSslErrors.isSelected());
+
+        environment.setProxyServerHost(proxyHost.getText());
+        environment.setProxyServerPort(Integer.parseInt(proxyPort.getText()));
+        environment.setProxyServerType((ProxyType) proxyType.getSelectedItem());
+        environment.setProxyUsername(proxyUsername.getText());
+        environment.setProxyPassword(new String(proxyPassword.getPassword()));
+
         environment.setPropertiesFilePath(properties.getText());
         environment.setAwtColor(colorLabel.getBackground());
         if (environment.getColor() == null) {
@@ -122,6 +153,13 @@ public class EnvironmentEditorDialog extends DialogWrapper {
         username.setText(environment.getUsername());
         password.setText(environment.getPassword());
         ignoreSslErrors.setSelected(environment.isIgnoreSslErrors());
+
+        proxyHost.setText(environment.getProxyServerHost());
+        proxyPort.setText(environment.getProxyServerPort() != null ? environment.getProxyServerPort().toString() : null);
+        proxyType.setSelectedItem(environment.getProxyServerType());
+        proxyUsername.setText(environment.getProxyUsername());
+        proxyPassword.setText(environment.getProxyPassword());
+
         properties.setText(environment.getPropertiesFilePath());
         colorLabel.setBackground(environment.getAwtColor());
     }
