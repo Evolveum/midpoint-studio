48ecd54320fbe2032c499978ed7cd7b9276a8ed7
Pavol Mederly <mederly@evolveum.com>
2020-09-25 17:38:18 +0200

    Add dark theme colors
    (But they do not work, yet.)

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/trace/Colors.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/trace/Colors.java
@@ -1,5 +1,6 @@
 package com.evolveum.midpoint.studio.ui.trace;
 
+import com.intellij.ui.Gray;
 import com.intellij.ui.JBColor;
 
 import java.awt.*;
@@ -9,15 +10,37 @@ import java.awt.*;
  */
 public class Colors {
 
-    public static final Color INPUT_1_COLOR = new Color(255, 255, 210);
-    public static final Color INPUT_2_COLOR = new Color(255, 255, 180);
-    public static final Color INPUT_3_COLOR = new Color(255, 255, 150);
-    public static final Color OUTPUT_1_COLOR = new Color(210, 255, 255);
-    public static final Color OUTPUT_2_COLOR = new Color(180, 255, 255);
-    public static final Color OUTPUT_2_COLOR_WEAK = new Color(235, 255, 255);
-    public static final Color OUTPUT_3_COLOR = new Color(150, 255, 255);
+    public static final Color INPUT_1_COLOR = new JBColor(
+            new Color(255, 255, 210),
+            new Color(60, 60, 0));
 
-    public static final Color CONFIGURATION_1_COLOR = new Color(200, 200, 200);
+    public static final Color INPUT_2_COLOR = new JBColor(
+            new Color(255, 255, 180),
+            new Color(90, 90, 0));
+
+    public static final Color INPUT_3_COLOR = new JBColor(
+            new Color(255, 255, 150),
+            new Color(120, 120, 0));
+
+    public static final Color OUTPUT_1_COLOR = new JBColor(
+            new Color(210, 255, 255),
+            new Color(0, 60, 60));
+
+    public static final Color OUTPUT_2_COLOR = new JBColor(
+            new Color(180, 255, 255),
+            new Color(0, 90, 90));
+
+    public static final Color OUTPUT_2_COLOR_WEAK = new JBColor(
+            new Color(235, 255, 255),
+            new Color(0, 40, 40));
+
+    public static final Color OUTPUT_3_COLOR = new JBColor(
+            new Color(150, 255, 255),
+            new Color(0, 120, 120));
+
+    public static final Color CONFIGURATION_1_COLOR = new JBColor(
+            Gray._200,
+            Gray._80);
 
     public static final String DISABLED_COLOR = "#c0c0c0"; // todo
 }
