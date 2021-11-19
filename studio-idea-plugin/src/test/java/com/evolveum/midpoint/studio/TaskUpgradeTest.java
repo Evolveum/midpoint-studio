30d376444d7c6d0d2d7a963a0b2608d3bef2ec3b
Viliam Repan <vilo.repan@evolveum.com>
2021-11-19 17:03:15 +0100

    attempt to fix logging in tests (not fully working yet)

--- studio-idea-plugin/src/test/java/com/evolveum/midpoint/studio/TaskUpgradeTest.java
+++ studio-idea-plugin/src/test/java/com/evolveum/midpoint/studio/TaskUpgradeTest.java
@@ -1,88 +1,37 @@
 package com.evolveum.midpoint.studio;
 
-import com.evolveum.midpoint.schema.SchemaConstantsGenerated;
-import org.bouncycastle.util.Arrays;
-import org.junit.jupiter.api.Disabled;
+import com.evolveum.midpoint.studio.util.MidPointUtils;
+import com.intellij.openapi.diagnostic.Logger;
+import org.apache.xalan.xslt.EnvironmentCheck;
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
 
-import javax.xml.namespace.NamespaceContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathFactory;
 import java.io.File;
-import java.util.Iterator;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.file.Files;
 
 /**
  * Created by Viliam Repan (lazyman).
  */
 public class TaskUpgradeTest {
 
-    @Disabled
+    private static final Logger LOG = Logger.getInstance(TaskUpgradeTest.class);
+
     @Test
-    public void transformTask() {
-        try {
-//            EnvironmentCheck check = new EnvironmentCheck();
-//            check.checkEnvironment(new PrintWriter(System.out));
-//
-//            System.out.println();
+    public void transformTask() throws Exception {
+        StringWriter sw = new StringWriter();
+        EnvironmentCheck check = new EnvironmentCheck();
+        check.checkEnvironment(new PrintWriter(sw));
 
-            File parent = new File(".");
-            File stylesheet = new File(parent, "src/main/resources/task-transformation.xslt");
-            File datafile = new File(parent, "src/test/testData/task-upgrade/ls-1-input.xml");
+        LOG.info(sw.toString());
 
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(true);
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.parse(datafile);
-            doc.normalize();
+        File parent = new File(".");
+        File datafile = new File(parent, "src/test/testData/task-upgrade/ls-1-input.xml");
 
-            XPathFactory xpf = XPathFactory.newInstance();
-            XPath xpath = xpf.newXPath();
-            xpath.setNamespaceContext(new NamespaceContext() {
+        String input = Files.readString(datafile.toPath());
 
-                @Override
-                public String getNamespaceURI(String prefix) {
-                    return "c".equals(prefix) ? SchemaConstantsGenerated.NS_COMMON : null;
-                }
+        String output = MidPointUtils.upgradeTaskToUseActivities(input);
 
-                @Override
-                public String getPrefix(String namespaceURI) {
-                    return SchemaConstantsGenerated.NS_COMMON.equals(namespaceURI) ? "c" : null;
-                }
-
-                @Override
-                public Iterator<String> getPrefixes(String namespaceURI) {
-                    return SchemaConstantsGenerated.NS_COMMON.equals(namespaceURI) ? new Arrays.Iterator(new String[]{"c"}) : null;
-                }
-            });
-
-            //Get first match
-            Boolean exists = (Boolean) xpath.evaluate("/c:task/c:activity", doc, XPathConstants.BOOLEAN);
-            if (exists) {
-                System.out.println("unchanged");
-                return;
-            }
-
-            StreamSource xsl = new StreamSource(stylesheet);
-
-            TransformerFactory tf = TransformerFactory.newInstance();
-            Transformer trans = tf.newTransformer(xsl);
-            trans.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-            trans.setParameter(OutputKeys.INDENT, "yes");
-            trans.setParameter(OutputKeys.ENCODING, "utf-8");
-
-            trans.transform(new DOMSource(doc), new StreamResult(System.out));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        LOG.info(output);
     }
 }
