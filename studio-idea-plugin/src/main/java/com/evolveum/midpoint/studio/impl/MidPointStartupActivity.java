ddeeee3724f3a9a83eb1217a4de8709454a01b1d
Viliam Repan <vilo.repan@evolveum.com>
2022-03-01 15:11:02 +0100

    MID-7695 namespace variants missing fix

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/impl/MidPointStartupActivity.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/impl/MidPointStartupActivity.java
@@ -1,17 +1,12 @@
 package com.evolveum.midpoint.studio.impl;
 
 import com.evolveum.midpoint.prism.PrismContext;
-import com.evolveum.midpoint.studio.MidPointConstants;
+import com.evolveum.midpoint.studio.impl.lang.codeInsight.NonexistentNamespaceUriCompletionProvider;
 import com.evolveum.midpoint.studio.util.MidPointUtils;
 import com.intellij.codeInspection.ex.InspectionProfileImpl;
 import com.intellij.codeInspection.ex.ToolsImpl;
-import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManager;
 import com.intellij.javaee.ExternalResourceManagerEx;
-import com.intellij.notification.BrowseNotificationAction;
-import com.intellij.notification.NotificationType;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.profile.codeInspection.ProjectInspectionProfileManager;
@@ -25,12 +20,6 @@ import java.util.*;
 public class MidPointStartupActivity implements StartupActivity {
 
     private static final Logger LOG = Logger.getInstance(MidPointStartupActivity.class);
-
-    private static final String[] IGNORED_RESOURCES = {
-            "http://midpoint.evolveum.com/xml/ns/public/common/org-3",
-            "http://prism.evolveum.com/xml/ns/public/matching-rule-3",
-            "http://midpoint.evolveum.com/xml/ns/public/resource/instance-3"
-    };
 
     @Override
     public void runActivity(@NotNull Project project) {
@@ -46,7 +35,7 @@ public class MidPointStartupActivity implements StartupActivity {
 
         List<String> toAdd = new ArrayList<>();
 
-        for (String s : IGNORED_RESOURCES) {
+        for (String s : NonexistentNamespaceUriCompletionProvider.IGNORED_RESOURCES) {
             if (set.contains(s)) {
                 continue;
             }
@@ -62,27 +51,5 @@ public class MidPointStartupActivity implements StartupActivity {
         if (tool != null) {
             tool.setEnabled(false);
         }
-
-        showVersioningChangeNotification(project);
-    }
-
-    private void showVersioningChangeNotification(Project project) {
-        IdeaPluginDescriptor descriptor = PluginManager.getInstance().findEnabledPlugin(PluginId.getId(MidPointConstants.PLUGIN_ID));
-        if (descriptor == null) {
-            return;
-        }
-
-        String version = descriptor.getVersion();
-        if (version.matches("\\d\\.\\d\\.\\d.*")) {
-            return;
-        }
-
-        MidPointUtils.publishNotification(project, "VERSION_CHANGE", "Release cycle changes",
-                "Release cycle for MidPoint Studio plugin will change with release 4.4. " +
-                        "There will be only two channels snapshot and stable (default).\n" +
-                        "If you're using nightly/milestone channel, please uninstall MidPoint Studio plugin, switch to 'snapshot' channel and install it again. " +
-                        "You will not loose any project environment configurations.",
-                NotificationType.INFORMATION,
-                new BrowseNotificationAction("More info...", "https://docs.evolveum.com/midpoint/tools/studio/builds/"));
     }
 }
