bd434ff6e76eba6dfc1c786d5cbdd6cbd67f7424
Viliam Repan <vilo.repan@evolveum.com>
2022-08-24 21:33:40 +0200

    updated gradle, fixed velocity dependencies, updated okhttp3 to 4.10.0

--- studio-idea-plugin/build.gradle.kts
+++ studio-idea-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
     id("org.jetbrains.intellij") version "1.8.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
@@ -17,7 +17,7 @@ plugins {
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
     id("io.gitlab.arturbosch.detekt") version "1.21.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
     // git plugin - read more: https://github.com/palantir/gradle-git-version
     id("com.palantir.git-version") version "0.12.2"
 }
@@ -66,6 +66,7 @@ dependencies {
         exclude("ch.qos.logback")
     }
     implementation(libs.asciidoctorj.tabbed.code)
+    implementation("org.apache.velocity", "velocity-engine-core")
 
     implementation(libs.openkeepass)
     implementation(libs.commons.lang)
