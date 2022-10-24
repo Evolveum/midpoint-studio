7796dc7ada78dacea46d13596fda575126d6b054
Viliam Repan <vilo.repan@evolveum.com>
2022-10-24 15:01:21 +0200

    more components will now not load (editors) when midpoint facet is not available

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/delta/ObjectDeltaPanel.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/delta/ObjectDeltaPanel.java
@@ -23,6 +23,7 @@ import com.intellij.diff.requests.DiffRequest;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.Separator;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -41,6 +42,8 @@ import java.util.List;
  * Created by Viliam Repan (lazyman).
  */
 public class ObjectDeltaPanel extends BorderLayoutPanel implements Disposable {
+
+    private static final Logger LOG = Logger.getInstance(ObjectDeltaPanel.class);
 
     public enum DiffStrategy {
 
@@ -136,8 +139,21 @@ public class ObjectDeltaPanel extends BorderLayoutPanel implements Disposable {
                     DiffObjectType first = diff.getFirstObject();
                     DiffObjectType second = diff.getSecondObject();
 
-                    PrismObject local = first.getObject().asPrismObject();
-                    PrismObject remote = second.getObject().asPrismObject();
+                    if (first == null || first.getObject() == null) {
+                        LOG.debug("First object not defined in DiffObjectType");
+                        return;
+                    }
+
+                    if (second == null) {
+                        LOG.debug("Second object not defined in DiffObjectType");
+                        return;
+                    }
+
+                    ObjectType firstObject = first.getObject();
+                    ObjectType secondObject = second.getObject();
+
+                    PrismObject local = firstObject.asPrismObject();
+                    PrismObject remote = secondObject != null ? secondObject.asPrismObject() : null;
 
                     PrismObject o1 = local;
                     PrismObject o2 = local.clone();
