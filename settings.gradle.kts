3f0e7fe7e5d39330dd8c59ea9c1b3ee426213148
Viliam Repan <vilo.repan@evolveum.com>
2023-11-29 21:43:49 +0100

    updated midpoint libs to 4.9-SNAPSHOT on master, also updated plugin version to 4.9.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
             version("jaxb-runtime", "2.3.2")
             version("jcommander", "1.81")
             version("logback", "1.2.3")
-            version("midpoint", "4.8")
+            version("midpoint", "4.9-SNAPSHOT")
             version("okhttp", "4.10.0")
             version("openkeepass", "0.8.1")
             version("spring", "5.3.8")
