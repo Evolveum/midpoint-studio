645d0415c62362c07a6221d0cf1637eed4d5f1dc
Viliam Repan <vilo.repan@evolveum.com>
2024-06-17 11:06:19 +0200

    save and download action in cleanup dialog implemented

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/impl/MissingReferencesNotificationAction.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/impl/MissingReferencesNotificationAction.java
@@ -1,6 +1,6 @@
 package com.evolveum.midpoint.studio.impl;
 
-import com.evolveum.midpoint.studio.impl.configuration.*;
+import com.evolveum.midpoint.studio.impl.configuration.MissingRefObject;
 import com.evolveum.midpoint.studio.ui.cleanup.MissingObjectRefsDialog;
 import com.evolveum.midpoint.studio.ui.cleanup.MissingRefKey;
 import com.evolveum.midpoint.studio.ui.cleanup.MissingRefUtils;
@@ -10,115 +10,40 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MissingReferencesNotificationAction extends NotificationAction {
 
     private static final String TEXT = "Configure missing...";
 
-    private final List<MissingRefObject> data;
+    private final List<MissingRefObject> foundRefs;
 
     /**
-     * @param data list of missing references obtained from cleanup task result.
+     * @param foundRefs list of missing references obtained from cleanup task result.
      */
-    public MissingReferencesNotificationAction(@NotNull List<MissingRefObject> data) {
+    public MissingReferencesNotificationAction(@NotNull List<MissingRefObject> foundRefs) {
         super(TEXT);
 
-        this.data = data;
+        this.foundRefs = foundRefs;
     }
 
     @Override
     public void actionPerformed(AnActionEvent e, Notification notification) {
         Project project = e.getProject();
 
-        List<MissingRefObject> cloned = data.stream()
-                .map(MissingRefObject::copy)
-                .collect(Collectors.toList());
+        List<MissingRefObject> clonedFoundRefs =
+                MissingRefUtils.populateCurrentActionsForMissingRefs(project, foundRefs);
 
-        CleanupService cs = CleanupService.get(project);
-        MissingRefObjects settings = cs.getSettings().getMissingReferences();
-
-        Map<MissingRefKey, MissingRefObject> existing = settings.getObjects().stream()
-                .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o));
-
-        for (MissingRefObject clonedObject : cloned) {
-            MissingRefKey key = new MissingRefKey(clonedObject.getOid(), clonedObject.getType());
-            MissingRefObject existingObject = existing.get(key);
-
-            Map<MissingRefKey, MissingRefAction> existingRefs = existingObject != null ?
-                    existingObject.getReferences().stream()
-                            .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o.getAction())) :
-                    new HashMap<>();
-
-            clonedObject.getReferences().forEach(mr -> {
-                MissingRefKey refKey = new MissingRefKey(mr.getOid(), mr.getType());
-                MissingRefAction action = existingRefs.get(refKey);
-                if (action != null) {
-                    mr.setAction(action);
-                }
-            });
-        }
-
-        MissingObjectRefsDialog dialog = new MissingObjectRefsDialog(project, cloned);
+        MissingObjectRefsDialog dialog = new MissingObjectRefsDialog(project, clonedFoundRefs);
         if (!dialog.showAndGet()) {
             return;
         }
 
         List<MissingRefObject> result = dialog.getData();
 
-        saveSettings(project, cloned, result);
-    }
+        List<MissingRefKey> removed = MissingRefUtils.computeRemovedRefKeys(clonedFoundRefs, result);
 
-    /**
-     * Save settings to project configuration file (midpoint.xml).
-     * <p>
-     * Replaces existing {@link MissingRefObject} object with the ones from result list.
-     * Removes {@link MissingRefObject} objects that were removed in the editor dialog by matching against input.
-     */
-    private void saveSettings(Project project, List<MissingRefObject> input, List<MissingRefObject> result) {
-        Set<MissingRefKey> resultKeys = result.stream()
-                .map(o -> new MissingRefKey(o.getOid(), o.getType()))
-                .collect(Collectors.toSet());
-
-        List<MissingRefObject> cloned = MissingRefUtils.cloneAndRemoveNonActionable(result);
-
-        CleanupService cs = CleanupService.get(project);
-
-        CleanupConfiguration config = cs.getSettings();
-        MissingRefObjects objects = config.getMissingReferences();
-
-        Map<MissingRefKey, MissingRefObject> existing = objects.getObjects().stream()
-                .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o));
-
-        // replace objects in settings with the ones from the dialog
-        for (MissingRefObject object : cloned) {
-            MissingRefKey key = new MissingRefKey(object.getOid(), object.getType());
-
-            MissingRefObject existingObject = existing.get(key);
-            if (existingObject != null) {
-                objects.getObjects().remove(existingObject);
-            }
-
-            objects.getObjects().add(object);
-        }
-
-        // remove objects from settings that were removed in the dialog
-        for (MissingRefObject object : input) {
-            MissingRefKey key = new MissingRefKey(object.getOid(), object.getType());
-
-            if (!resultKeys.contains(key)) {
-                MissingRefObject existingObject = existing.get(key);
-                if (existingObject != null) {
-                    objects.getObjects().remove(existingObject);
-                }
-            }
-        }
-
-        cs.settingsUpdated();
+        MissingRefUtils.updateMissingRefSettings(project, result, removed);
     }
 
     @Override
--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/cleanup/MissingRefUtils.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/cleanup/MissingRefUtils.java
@@ -80,4 +80,107 @@ public class MissingRefUtils {
 
         return download;
     }
+
+    /**
+     * Update settings (in midpoint.xml) for missing ref objects defined in missingRefObjects parameter.
+     * <p>
+     * Replaces existing {@link MissingRefObject} object with the ones from missingRefObjects list.
+     * Removes {@link MissingRefObject} objects that were removed in the editor dialog by matching against input.
+     */
+    public static void updateMissingRefSettings(
+            Project project, List<MissingRefObject> missingRefObjects, List<MissingRefKey> removed) {
+
+        List<MissingRefObject> cloned = MissingRefUtils.cloneAndRemoveNonActionable(missingRefObjects);
+
+        CleanupService cs = CleanupService.get(project);
+
+        CleanupConfiguration config = cs.getSettings();
+        MissingRefObjects objects = config.getMissingReferences();
+
+        Map<MissingRefKey, MissingRefObject> existing = objects.getObjects().stream()
+                .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o));
+
+        // replace objects in settings with the ones from the dialog
+        for (MissingRefObject object : cloned) {
+            MissingRefKey key = new MissingRefKey(object.getOid(), object.getType());
+
+            MissingRefObject existingObject = existing.get(key);
+            if (existingObject != null) {
+                objects.getObjects().remove(existingObject);
+            }
+
+            objects.getObjects().add(object);
+        }
+
+        removed.forEach(key -> {
+            MissingRefObject existingObject = existing.get(key);
+            if (existingObject != null) {
+                objects.getObjects().remove(existingObject);
+            }
+        });
+
+        cs.settingsUpdated();
+    }
+
+    /**
+     * Finds action in cleanup settings for given missing references - if there's any
+     * and populates the action in the missing reference object.
+     *
+     * @param project
+     * @param missing
+     * @return cloned list of missing references with actions populated from cleanup settings
+     */
+    public static List<MissingRefObject> populateCurrentActionsForMissingRefs(
+            Project project, List<MissingRefObject> missing) {
+
+        List<MissingRefObject> clonedMissing = missing.stream()
+                .map(MissingRefObject::copy)
+                .collect(Collectors.toList());
+
+        CleanupService cs = CleanupService.get(project);
+        MissingRefObjects settings = cs.getSettings().getMissingReferences();
+
+        Map<MissingRefKey, MissingRefObject> existing = settings.getObjects().stream()
+                .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o));
+
+        for (MissingRefObject clonedObject : clonedMissing) {
+            MissingRefKey key = new MissingRefKey(clonedObject.getOid(), clonedObject.getType());
+            MissingRefObject existingObject = existing.get(key);
+
+            Map<MissingRefKey, MissingRefAction> existingRefs = existingObject != null ?
+                    existingObject.getReferences().stream()
+                            .collect(Collectors.toMap(o -> new MissingRefKey(o.getOid(), o.getType()), o -> o.getAction())) :
+                    new HashMap<>();
+
+            clonedObject.getReferences().forEach(mr -> {
+                MissingRefKey refKey = new MissingRefKey(mr.getOid(), mr.getType());
+                MissingRefAction action = existingRefs.get(refKey);
+                if (action != null) {
+                    mr.setAction(action);
+                }
+            });
+        }
+
+        return clonedMissing;
+    }
+
+    /**
+     * Computes list of {@link MissingRefKey} removed objects from output list compare to input list
+     *
+     * @param input  list of {@link MissingRefObject}
+     * @param output list of {@link MissingRefObject} that was modified (may not contain all objects from input list)
+     * @return list of {@link MissingRefKey keys representing objects removed from input list
+     */
+    public static List<MissingRefKey> computeRemovedRefKeys(
+            List<MissingRefObject> input, List<MissingRefObject> output) {
+
+        Set<MissingRefKey> resultRefKeys = output.stream()
+                .map(o -> new MissingRefKey(o.getOid(), o.getType()))
+                .collect(Collectors.toSet());
+
+        return input.stream()
+                .map(o -> new MissingRefKey(o.getOid(), o.getType()))
+                .filter(k -> !resultRefKeys.contains(k))
+                .toList();
+    }
 }
