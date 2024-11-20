d1a018b1606145668cfffc9d7fc1baa2a3394308
Viliam Repan <vilo.repan@evolveum.com>
2024-11-20 17:46:08 +0100

    fixed midscribe dependency (switched to 4.9-snapshot)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
 
             // versions of midpoint libraries
             version("midpoint", "4.9")
-            version("midscribe", "4.10-SNAPSHOT")
+            version("midscribe", "4.9-SNAPSHOT")
 
             // versions of third-party libraries
             version("annotations", "24.1.0")
