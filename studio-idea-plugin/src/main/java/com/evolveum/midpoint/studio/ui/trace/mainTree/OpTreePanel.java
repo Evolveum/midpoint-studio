873fd8a79303a74c8b0a8ffc85b1a735780c6f37
Viliam Repan <vilo.repan@evolveum.com>
2026-06-03 16:51:00 +0200

    Moved trace options to main OpTreePanel

--- studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/trace/mainTree/OpTreePanel.java
+++ studio-idea-plugin/src/main/java/com/evolveum/midpoint/studio/ui/trace/mainTree/OpTreePanel.java
@@ -1,19 +1,22 @@
 package com.evolveum.midpoint.studio.ui.trace.mainTree;
 
 import com.evolveum.midpoint.schema.traces.OpNode;
+import com.evolveum.midpoint.schema.traces.OpType;
+import com.evolveum.midpoint.schema.traces.PerformanceCategory;
 import com.evolveum.midpoint.studio.impl.MidPointProjectNotifier;
 import com.evolveum.midpoint.studio.impl.trace.Options;
 import com.evolveum.midpoint.studio.impl.trace.TraceService;
 import com.evolveum.midpoint.studio.ui.trace.lens.TraceTreeViewColumn;
 import com.evolveum.midpoint.studio.ui.trace.mainTree.model.AbstractOpTreeTableNode;
 import com.evolveum.midpoint.studio.ui.trace.mainTree.model.OpTreeTableModel;
+import com.evolveum.midpoint.studio.ui.trace.options.PredefinedColumnSet;
+import com.evolveum.midpoint.studio.ui.trace.options.PredefinedOpTypeSet;
+import com.evolveum.midpoint.studio.ui.trace.options.PredefinedPerformanceCategoriesSet;
 import com.evolveum.midpoint.studio.ui.treetable.DefaultColumnInfo;
 import com.evolveum.midpoint.studio.ui.treetable.DefaultTreeTable;
 import com.evolveum.midpoint.studio.util.MidPointUtils;
 import com.intellij.icons.AllIcons;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionToolbar;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.components.JBScrollPane;
@@ -38,6 +41,8 @@ public class OpTreePanel extends JPanel {
 
     private static final Logger LOG = Logger.getInstance(OpTreePanel.class);
 
+    private final Project project;
+
     private DefaultTreeTable<OpNode, OpTreeTableModel> traceTreeTable;
 
     private OpTreeTableModel opTreeTableModel;
@@ -55,6 +60,7 @@ public class OpTreePanel extends JPanel {
     public OpTreePanel(Project project, @Nullable OpNode rootOpNode) {
         super(new BorderLayout());
 
+        this.project = project;
         this.rootOpNode = rootOpNode;
 
         MessageBus bus = project.getMessageBus();
@@ -107,12 +113,234 @@ public class OpTreePanel extends JPanel {
         group.add(MidPointUtils.createAnAction("Show all children", AllIcons.Actions.ShowAsTree, e -> setChildrenVisible(true)));
         group.add(MidPointUtils.createAnAction("Hide selected", AllIcons.General.HideToolWindow, e -> hideSelected(false)));
         group.add(MidPointUtils.createAnAction("Hide selected (tree)", AllIcons.Actions.DeleteTagHover, e -> hideSelected(true)));
+        group.addSeparator();
+        group.add(createOpTypeFilterAction());
+        group.add(createCategoryFilterAction());
+        group.add(createColumnFilterAction());
 
         ActionToolbar toolbar = ActionManager.getInstance()
                 .createActionToolbar("TraceViewPanelMainToolbar", group, true);
         toolbar.setTargetComponent(this);
 
         return toolbar.getComponent();
+    }
+
+    private DefaultActionGroup createOpTypeFilterAction() {
+        DefaultActionGroup filterGroup = new DefaultActionGroup("Filter operation types", true);
+        filterGroup.getTemplatePresentation().setIcon(AllIcons.General.Filter);
+
+        for (PredefinedOpTypeSet set : PredefinedOpTypeSet.values()) {
+            filterGroup.add(new ToggleAction(set.toString()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    Collection<OpType> setTypes = set.getTypes();
+                    java.util.Set<OpType> typesToShow = TraceService.getInstance(project).getOptions().getTypesToShow();
+                    return typesToShow.size() == setTypes.size() && typesToShow.containsAll(setTypes);
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getTypesToShow().clear();
+                        options.getTypesToShow().addAll(set.getTypes());
+                    } else {
+                        options.getTypesToShow().removeAll(set.getTypes());
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        filterGroup.addSeparator();
+
+        for (OpType type : OpType.values()) {
+            filterGroup.add(new ToggleAction(type.getLabel()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    return TraceService.getInstance(project).getOptions().getTypesToShow().contains(type);
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getTypesToShow().add(type);
+                    } else {
+                        options.getTypesToShow().remove(type);
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        return filterGroup;
+    }
+
+    private DefaultActionGroup createCategoryFilterAction() {
+        DefaultActionGroup filterGroup = new DefaultActionGroup("Filter categories", true);
+        filterGroup.getTemplatePresentation().setIcon(AllIcons.General.Filter);
+
+        for (PredefinedPerformanceCategoriesSet set : PredefinedPerformanceCategoriesSet.values()) {
+            filterGroup.add(new ToggleAction(set.toString()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    Options opts = TraceService.getInstance(project).getOptions();
+                    Collection<PerformanceCategory> setCategories = set.getCategories();
+                    java.util.Set<PerformanceCategory> categoriesToShow = opts.getCategoriesToShow();
+                    return categoriesToShow.size() == setCategories.size()
+                            && categoriesToShow.containsAll(setCategories)
+                            && opts.isShowAlsoParents() == set.isShowParents();
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getCategoriesToShow().clear();
+                        options.getCategoriesToShow().addAll(set.getCategories());
+                        options.setShowAlsoParents(set.isShowParents());
+                    } else {
+                        options.getCategoriesToShow().removeAll(set.getCategories());
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        filterGroup.addSeparator();
+
+        filterGroup.add(new ToggleAction("Show also parents") {
+
+            @Override
+            public @NotNull ActionUpdateThread getActionUpdateThread() {
+                return ActionUpdateThread.EDT;
+            }
+
+            @Override
+            public boolean isSelected(@NotNull AnActionEvent e) {
+                return TraceService.getInstance(project).getOptions().isShowAlsoParents();
+            }
+
+            @Override
+            public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                Options options = TraceService.getInstance(project).getOptions().clone();
+                options.setShowAlsoParents(state);
+                TraceService.getInstance(project).setOptions(options);
+            }
+        });
+
+        filterGroup.addSeparator();
+
+        for (PerformanceCategory category : PerformanceCategory.values()) {
+            filterGroup.add(new ToggleAction(category.getLabel()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    return TraceService.getInstance(project).getOptions().getCategoriesToShow().contains(category);
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getCategoriesToShow().add(category);
+                    } else {
+                        options.getCategoriesToShow().remove(category);
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        return filterGroup;
+    }
+
+    private DefaultActionGroup createColumnFilterAction() {
+        DefaultActionGroup filterGroup = new DefaultActionGroup("Filter columns", true);
+        filterGroup.getTemplatePresentation().setIcon(AllIcons.Actions.SplitVertically);
+
+        for (PredefinedColumnSet set : PredefinedColumnSet.values()) {
+            filterGroup.add(new ToggleAction(set.toString()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    Collection<TraceTreeViewColumn> setColumns = set.getColumns();
+                    java.util.Set<TraceTreeViewColumn> columnsToShow = TraceService.getInstance(project).getOptions().getColumnsToShow();
+                    return columnsToShow.size() == setColumns.size() && columnsToShow.containsAll(setColumns);
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getColumnsToShow().clear();
+                        options.getColumnsToShow().addAll(set.getColumns());
+                    } else {
+                        options.getColumnsToShow().removeAll(set.getColumns());
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        filterGroup.addSeparator();
+
+        for (TraceTreeViewColumn column : TraceTreeViewColumn.values()) {
+            filterGroup.add(new ToggleAction(column.getName()) {
+
+                @Override
+                public @NotNull ActionUpdateThread getActionUpdateThread() {
+                    return ActionUpdateThread.EDT;
+                }
+
+                @Override
+                public boolean isSelected(@NotNull AnActionEvent e) {
+                    return TraceService.getInstance(project).getOptions().getColumnsToShow().contains(column);
+                }
+
+                @Override
+                public void setSelected(@NotNull AnActionEvent e, boolean state) {
+                    Options options = TraceService.getInstance(project).getOptions().clone();
+                    if (state) {
+                        options.getColumnsToShow().add(column);
+                    } else {
+                        options.getColumnsToShow().remove(column);
+                    }
+                    TraceService.getInstance(project).setOptions(options);
+                }
+            });
+        }
+
+        return filterGroup;
     }
 
     private void expandAll() {
